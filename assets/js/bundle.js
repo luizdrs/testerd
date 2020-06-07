9382396a1278e32d21d0c4fbfef00985ba8f2495
Luiz Diego Rodrigues dos Santos <66563839+luizdrs@users.noreply.github.com>
2020-06-07 19:48:31 -0300

    Todos os ajustes

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,6 +1,6 @@
 (() => {
-    const selector = selector => /* trecho omitido */
-    const create = element => /* trecho omitido */
+    const selector = selector => {return document.querySelector(selector)}/* trecho omitido FEITO */
+    const create = element => {return document.createElement(element)}/* trecho omitido FEITO */
 
     const app = selector('#app');
 
@@ -15,7 +15,7 @@
 
     Form.onsubmit = async e => {
         e.preventDefault();
-        const [email, password] = /* trecho omitido */
+        const [email, password] = e.target.children; /* trecho omitido FEITO */
 
         const {url} = await fakeAuthenticate(email.value, password.value);
 
@@ -32,9 +32,14 @@
             : button.removeAttribute('disabled');
     };
 
-    Form.innerHTML = /**
+    Form.innerHTML = 
+        '<input type="email" name="email" placeholder="Entre com seu e-mail">'+
+        '<input type="password" name="password" placeholder="Digite sua senha supersecreta">'+
+        '<button type="submit" disabled="disabled">Entrar</button>';
+    /**
     * bloco de código omitido
     * monte o seu formulário
+    * FEITO
     */
 
     app.appendChild(Logo);
@@ -45,42 +50,88 @@
         /**
          * bloco de código omitido
          * aqui esperamos que você faça a requisição ao URL informado
+         * FEITO
          */
 
+        var url = "http://www.mocky.io/v2/5dba690e3000008c00028eb6";
+
+        var http = new XMLHttpRequest();
+
+        http.open("GET", url, false);
+        http.send();
+
+        var data = JSON.parse(http.responseText);
+
         const fakeJwtToken = `${btoa(email+password)}.${btoa(data.url)}.${(new Date()).getTime()+300000}`;
-        /* trecho omitido */
+
+        /* trecho omitido 
+         * FEITO 
+        */
+        localStorage.setItem('token', fakeJwtToken);
 
         return data;
+
     }
 
     async function getDevelopersList(url) {
+
         /**
          * bloco de código omitido
          * aqui esperamos que você faça a segunda requisição 
          * para carregar a lista de desenvolvedores
+         * FEITO
          */
+
+        var http = new XMLHttpRequest();
+
+        http.open("GET", url, false);
+        http.send();
+        
+        var data = JSON.parse(http.responseText);
+
+        return data;
     }
 
     function renderPageUsers(users) {
         app.classList.add('logged');
-        Login.style.display = /* trecho omitido */
+        Login.style.display = 'none'/* trecho omitido FEITO */
 
         const Ul = create('ul');
         Ul.classList.add('container')
-
+        console.log(users);
         /**
          * bloco de código omitido
          * exiba a lista de desenvolvedores
+         * FEITO
          */
+       users.forEach(function (val) {
+        var card = create('li');
+        var avatar = create('img');
+        var name = create('span');
+        var link = create('a');
+        link.href = val.html_url;
+        avatar.src = val.avatar_url;
+        name.innerHTML = val.login;
+        link.appendChild(avatar);
+        link.appendChild(name);
+        card.appendChild(link);
+        Ul.appendChild(card);
+       });
 
         app.appendChild(Ul)
     }
 
     // init
     (async function(){
-        const rawToken = /* trecho omitido */
+        const rawToken = localStorage.getItem('token');/* trecho omitido FEITO*/
+        console.log(rawToken);
         const token = rawToken ? rawToken.split('.') : null
+        console.log(token);
         if (!token || token[2] < (new Date()).getTime()) {
+            if(token) {
+                console.log(token[2]);
+            }
+            console.log((new Date()).getTime());
             localStorage.removeItem('token');
             location.href='#login';
             app.appendChild(Login);
